0ca65fe3354e649a199f91ffec2f7bbcac7d1a87
Omid <omid.mousavi.1385@gmail.com>
2025-06-28 16:44:25 -0700

    Add files

---
+++ SocketServer/Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace SocketServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPort = new System.Windows.Forms.NumericUpDown();
+            this.listBoxClients = new System.Windows.Forms.ListBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBoxLog = new System.Windows.Forms.TextBox();
+            this.textBoxMessage = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.txtPort)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtPort
+            // 
+            this.txtPort.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPort.Location = new System.Drawing.Point(187, 30);
+            this.txtPort.Maximum = new decimal(new int[] {
+            9999,
+            0,
+            0,
+            0});
+            this.txtPort.Minimum = new decimal(new int[] {
+            1024,
+            0,
+            0,
+            0});
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(75, 26);
+            this.txtPort.TabIndex = 0;
+            this.txtPort.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtPort.Value = new decimal(new int[] {
+            8000,
+            0,
+            0,
+            0});
+            // 
+            // listBoxClients
+            // 
+            this.listBoxClients.FormattingEnabled = true;
+            this.listBoxClients.Location = new System.Drawing.Point(13, 168);
+            this.listBoxClients.Name = "listBoxClients";
+            this.listBoxClients.Size = new System.Drawing.Size(318, 95);
+            this.listBoxClients.TabIndex = 1;
+            this.listBoxClients.SelectedIndexChanged += new System.EventHandler(this.listBoxClients_SelectedIndexChanged);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Enabled = false;
+            this.btnSend.Location = new System.Drawing.Point(337, 227);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(269, 36);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(63, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(73, 26);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "127.0.0.1";
+            // 
+            // textBoxLog
+            // 
+            this.textBoxLog.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxLog.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxLog.Location = new System.Drawing.Point(13, 289);
+            this.textBoxLog.Multiline = true;
+            this.textBoxLog.Name = "textBoxLog";
+            this.textBoxLog.ReadOnly = true;
+            this.textBoxLog.Size = new System.Drawing.Size(593, 181);
+            this.textBoxLog.TabIndex = 5;
+            // 
+            // textBoxMessage
+            // 
+            this.textBoxMessage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxMessage.Enabled = false;
+            this.textBoxMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxMessage.Location = new System.Drawing.Point(337, 168);
+            this.textBoxMessage.Multiline = true;
+            this.textBoxMessage.Name = "textBoxMessage";
+            this.textBoxMessage.Size = new System.Drawing.Size(269, 53);
+            this.textBoxMessage.TabIndex = 6;
+            this.textBoxMessage.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 152);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "List of clients:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 273);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Log:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(334, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Send message to client:";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(6, 83);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(147, 36);
+            this.btnStart.TabIndex = 11;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnStop);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.btnStart);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtPort);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(15, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(309, 126);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Settings";
+            // 
+            // btnStop
+            // 
+            this.btnStop.Enabled = false;
+            this.btnStop.Location = new System.Drawing.Point(155, 83);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(147, 36);
+            this.btnStop.TabIndex = 15;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(32, 37);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(20, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "IP:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(152, 37);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Port:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(431, 42);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(66, 16);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "STATUS:";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.ForeColor = System.Drawing.Color.Red;
+            this.lblStatus.Location = new System.Drawing.Point(430, 68);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(104, 24);
+            this.lblStatus.TabIndex = 14;
+            this.lblStatus.Text = "INACTIVE";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(623, 482);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxMessage);
+            this.Controls.Add(this.textBoxLog);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.listBoxClients);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.txtPort)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown txtPort;
+        private System.Windows.Forms.ListBox listBoxClients;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBoxLog;
+        private System.Windows.Forms.TextBox textBoxMessage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ SocketServer/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace SocketServer
+{
+    public partial class Form1 : Form
+    {
+        
+        private TcpListener server;
+        private Thread serverThread;
+        private Dictionary<string, TcpClient> clients = new Dictionary<string, TcpClient>();
+
+        private bool serverRunning = false;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void StartServer(int port = 8000)
+        {
+            serverRunning = true;
+
+            server = new TcpListener(IPAddress.Any, port);
+            server.Start();
+            AppendLog("Server started on port " + port);
+
+            while (serverRunning)
+            {
+                if (server.Pending())
+                {
+                    // Check if there's a client waiting to connect
+                    TcpClient client = server.AcceptTcpClient();
+                    string clientId = Guid.NewGuid().ToString();
+                    clients[clientId] = client;
+
+                    Invoke(new Action(() => listBoxClients.Items.Add(clientId)));
+                    AppendLog(string.Format("Client connected: {0}", clientId));
+
+                    Thread clientThread = new Thread(() => HandleClient(client, clientId));
+                    clientThread.IsBackground = true;
+                    clientThread.Start();
+                }
+            }
+
+            // Stop the server after the loop exits
+            AppendLog("Server stopped.");
+            server.Stop();
+
+        }
+
+        private void HandleClient(TcpClient client, string clientId)
+        {
+            NetworkStream stream = client.GetStream();
+            byte[] buffer = new byte[1024];
+
+            try
+            {
+                while (true)
+                {
+                    int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                    if (bytesRead == 0) break;
+
+                    string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                    AppendLog(string.Format("From {0}: {1}", clientId, message));
+                }
+            }
+            catch (Exception ex)
+            {
+                AppendLog(string.Format("Error with client {0}: {1}", clientId, ex.Message));
+            }
+            finally
+            {
+                client.Close();
+                clients.Remove(clientId);
+                Invoke(new Action(() => listBoxClients.Items.Remove(clientId)));
+                AppendLog(string.Format("Client disconnected: {0}", clientId));
+            }
+        }
+
+        private void AppendLog(string message)
+        {
+            Invoke(new Action(() =>
+            {
+                textBoxLog.AppendText(string.Format("{0}: {1}{2}", DateTime.Now, message, Environment.NewLine));
+            }));
+        }
+
+
+        private void listBoxClients_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            btnSend.Enabled = listBoxClients.SelectedItem != null;
+            textBoxMessage.Enabled = listBoxClients.SelectedItem != null;
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if (listBoxClients.SelectedItem == null) return;
+
+            string clientId = listBoxClients.SelectedItem.ToString();
+
+            TcpClient client; if (clients.TryGetValue(clientId, out client))
+            {
+                NetworkStream stream = client.GetStream();
+                byte[] data = Encoding.UTF8.GetBytes(textBoxMessage.Text);
+                stream.Write(data, 0, data.Length);
+                AppendLog(string.Format("To {0}: {1}", clientId, textBoxMessage.Text));
+
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            btnStop.Enabled = true;
+            btnStart.Enabled = false;
+            txtPort.Enabled = false;
+
+            lblStatus.Text = "ACTIVE";
+            lblStatus.ForeColor = Color.Green;
+
+            serverThread = new Thread(() => StartServer(decimal.ToInt32(txtPort.Value)));
+            serverThread.IsBackground = true;
+            serverThread.Start();                        
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            btnStop.Enabled = false;
+            btnStart.Enabled = true;
+            txtPort.Enabled = true;
+
+            lblStatus.Text = "INACTIVE";
+            lblStatus.ForeColor = Color.Red;
+
+            serverRunning = false;
+            
+            foreach (var client in clients.Values)
+                client.Close();
+
+            server.Stop();
+
+
+            //serverThread.Abort();
+            //server.Stop();
+
+        }
+    }
+}
